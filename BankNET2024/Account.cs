e4ea3e3078bfc572c94057cbf287402b0f429cab
Tim Suvorov <timiniev@outlook.com>
2024-10-31 11:50:20 +0100

    abstract class fixed

--- BankNET2024/Account.cs
+++ BankNET2024/Account.cs
@@ -6,12 +6,16 @@ using System.Threading.Tasks;
 
 namespace BankNET2024
 {
-    public class Account
+    public abstract class Account
     {
         public string AccountNumber { get; set; }
-        public float Balance { get; set; }
+        public float ActualBalance { get; set; }
 
-        
+        public string Name { get; set; }
+        public int MyProperty { get; set; }
+        public float SavingAccount { get; set; }
+
+
 
     }
 }

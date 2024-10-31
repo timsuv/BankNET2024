6554daddc6da279d448558583cab284a9f443b4d
joel3 <joel3@JOEL>
2024-10-31 11:57:11 +0100

    Added constructor

--- BankNET2024/SavingAccount.cs
+++ BankNET2024/SavingAccount.cs
@@ -8,6 +8,9 @@ namespace BankNET2024
 {
     public class SavingAccount : Account
     {
+        public SavingAccount(string accountNumber, float balance, string name, string contactInfo) : base(accountNumber, balance, name, contactInfo)
+        {
 
+        }
     }
 }

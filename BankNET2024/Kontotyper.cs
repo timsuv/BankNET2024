b603c95779bc1aa46acf647ffa79d6404a8da03e
Sami Harun <sami.harun@hotmail.com>
2024-10-31 14:31:35 +0100

    done

--- BankNET2024/Kontotyper.cs
+++ BankNET2024/Kontotyper.cs
@@ -12,13 +12,15 @@ namespace BankNET2024
         public string AccountNumber { get; set; }
         public float Balance { get; set; }
         public string Currency { get; set; }
+        public float IntrestRate { get; set; }
 
-        public AccountType(string accountnumber, float balance, string currency)
+        public AccountType(string accountnumber, float balance, string currency, float intrestrate)
 
         {
             AccountNumber = accountnumber;
             Balance = balance;
             Currency = currency;
+            IntrestRate = intrestrate;
         }
 
         // insättning
@@ -48,9 +50,44 @@ namespace BankNET2024
     }
     public class SavingsAccount : AccountType
     {
+        public float InterestRate { get; set; }
+
+        //konstructor
+        public SavingsAccount(string accountNumber, float balance, string currency, float interestRate) : base(accountNumber, balance, currency, interestRate)
+        {
+            InterestRate = interestRate;
+        }
+
+        // Beräknar årliga räntan baserat på saldo
+        public float CalculateAnnualInterest()
+        {
+            return Balance * (InterestRate / 100);
+        }
+
+        public void ShowLoanDetails()
+        {
+            base.ShowAccountDetails();
+            Console.WriteLine($"Ränta: {InterestRate}%");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         // Sparkonto (årlig ränta)
         
         //Lånekonto
         
     }
 }
+

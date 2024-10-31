162fd9cb6581e62e81d7f4a326446737ac5619a5
Sami Harun <sami.harun@hotmail.com>
2024-10-31 12:30:52 +0100

    kontotyper

---
+++ BankNET2024/Kontotyper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankNET2024
+{
+    public class AccountType
+    {
+        //Basklasser
+        public string AccountNumber;
+        public decimal TotalBalance;
+        public string Currency;
+
+        public AccountType(string accountnumber, decimal totalbalance, string currency) 
+        
+        {
+            AccountNumber = accountnumber;
+            TotalBalance = totalbalance;
+            Currency = currency;
+        }
+
+        // insättning
+        public void Deposit(Decimal amount)
+        {
+            TotalBalance += amount;
+        }
+
+        // Uttag från konto, returnerar false om uttaget överstiger saldot
+        public bool Withdraw(decimal amount)
+        {
+            if (TotalBalance >= amount)
+            {
+                TotalBalance -= amount;
+                return true;
+            }
+            return false;
+        }
+
+
+
+
+        // Visar  kontoinformation
+
+
+
+        // Sparkonto som har en årlig ränta
+
+
+
+    }
+}

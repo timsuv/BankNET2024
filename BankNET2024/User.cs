7ff2b3fbd53a7a448c0bc4b2f63484d2a4a3205b
Tim Suvorov <timiniev@outlook.com>
2024-11-05 11:36:04 +0100

    new master, base

---
+++ BankNET2024/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankNET2024
+{
+    public class User
+    {
+        public User(string username, string password, string firstName, string lastName, string phoneNumber, decimal salary, List<Account> accounts)
+        {
+            Username = username;
+            Password = password;
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Salary = salary;
+            Accounts = accounts;
+        }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public decimal Salary { get; set; }
+        public List<Account> Accounts { get; set; }
+
+        //  public bool IsLocked { get; set; } kommer senare
+        //public int Attempts { get; set; }
+
+        public void CreateOwnAccout()
+        {
+            //Accounts.Add(new Account(Account );
+        }
+        public void DisplayAccounts()
+        {
+            //visar infos om alla accounts med bara nummer och balance
+            Console.WriteLine("");
+        }
+    }
+
+
+}

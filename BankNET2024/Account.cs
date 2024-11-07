718f2176e425ad1d0f1eb8c7962978729429c8a4
joel3 <joel3@JOEL>
2024-11-07 11:16:39 +0100

    withdraw/deposit

--- BankNET2024/Account.cs
+++ BankNET2024/Account.cs
@@ -31,13 +31,62 @@ namespace BankNET2024
         public string Password { get; set; }
 
 
-        public void Deposit()
+        public void Deposit(List<Account> accounts)
         {
-            Balance += Amount;
+            Console.WriteLine("\nVilket konto vill du dra ut från??");
+            string accountNumber = Console.ReadLine();
+
+            Account toAccount = accounts.FirstOrDefault(a => a.AccountNumber == accountNumber);
+
+            if (toAccount != null)
+            {
+                Console.WriteLine("\nAnge mängden pengar. ");
+                decimal amount = decimal.Parse(Console.ReadLine());
+
+                if (amount < 0)
+                {
+                    Balance += amount;
+                    toAccount.Balance += amount;
+                    var log = $"Mängden pengar inlagd: {amount} på {AccountNumber}";
+                    Console.WriteLine(log);
+                    Transaction transaction = new Transaction(DateTime.Now, new List<string> { log });
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Uttag misslyckades. Detta bankkonto existerar inte.");
+            }
         }
-        public void Withdraw()
+        public void Withdraw(List<Account> accounts)
         {
-            Balance -= Amount;
+            Console.WriteLine("\nVilket konto vill du dra ut från??");
+            string accountNumber = Console.ReadLine();
+
+            Account toAccount = accounts.FirstOrDefault(a => a.AccountNumber == accountNumber);
+
+            if (toAccount != null)
+            {
+                Console.WriteLine("\nAnge mängden pengar. ");
+                decimal amount = decimal.Parse(Console.ReadLine());
+
+                if (Balance >= amount && amount < 0)
+                {
+                    Balance -= amount;
+                    toAccount.Balance += amount;
+                    var log = $"Mängden pengar uttagen: {amount} från {AccountNumber}";
+                    Console.WriteLine(log);
+                    Transaction transaction = new Transaction(DateTime.Now, new List<string> { log });
+                }
+                else
+                {
+                    Console.WriteLine("Uttag misslyckades. Inte tillräckligt med pengar.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Uttag misslyckades. Detta bankkonto existerar inte.");
+            }
         }
 
         public void DisplayAccount()
@@ -56,7 +105,7 @@ namespace BankNET2024
                 Console.WriteLine("\nEnter the amount to transfer:");
                 decimal amount = decimal.Parse(Console.ReadLine());
 
-                if (Balance >= amount)
+                if (Balance >= amount && amount < 0)
                 {
                     Balance -= amount;
                     toAccount.Balance += amount;
@@ -73,40 +122,6 @@ namespace BankNET2024
             {
                 Console.WriteLine("Transfer failed. Invalid account number.");
             }
-        }
-
-        
-        private void CreateAccount()
-        {
-            var newAccount = new Account(
-                accountNumber: "1",
-                balance: 50036.32M,
-                name: "Bob Åberg",
-                contactInfo: "bob@gmail.com, 0728539675",
-                amount: 0M,
-                password: "apa1"
-            );
-
-            var newAccount2 = new Account(
-            accountNumber: "22",
-            balance: 40036.32M,
-            name: "Emil Lönneberga",
-            contactInfo: "Emil.233@gmail.com, 0721939396",
-            amount: 0M,
-            password: "apa2"
-        );
-            var newAccount3 = new Account(
-                accountNumber: "333",
-                balance: 30036.32M,
-                name: "Anders Strömberg",
-                contactInfo: "Anders.213@gmail.com, 0721602756",
-                amount: 0M,
-                password: "apa3"
-);
-
-            Accounts.Add(newAccount);
-            Accounts.Add(newAccount2);
-            Accounts.Add(newAccount3);
         }
 
     }

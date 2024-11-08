4b42a8277f968ef6d32a03b7a4f79a900c2ea938
Tim Suvorov <timsuvorov@icloud.com>
2024-11-08 14:01:52 +0100

    open foreign account fixed

---
+++ BankNET2024/Currency.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankNET2024
+{
+    public enum Currency
+    {
+        USD,
+        EUR,
+        GBP,
+        JPY,
+        CNY
+
+    }
+}
